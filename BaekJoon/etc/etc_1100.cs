86e50d6fdacad9fb98b3ffa90aed3379a854a9e1
TryingPop <qotjdgns2001@gmail.com>
2024-11-08 18:51:20 +0900

    fix: readme 최신화 2024. 11 최신화

--- BaekJoon/etc/etc_1100.cs
+++ BaekJoon/etc/etc_1100.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+날짜 : 2024. 11. 8
+이름 : 배성훈
+내용 : 재활용 캠페인
+    문제번호 : 22988번
+
+
+*/
+
 namespace BaekJoon.etc
 {
-    internal class etc_1099
+    internal class etc_1100
     {
     }
 }
