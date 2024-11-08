23b6f6adc2c4ff680c197bd07a114f1b6bf92440
TryingPop <qotjdgns2001@gmail.com>
2024-11-08 13:54:24 +0900

    feat: 백준 연습용 문제 2024. 11 Study
    그리디, 누적합 문제다

---
+++ BaekJoon/10_000~19_999/18_000~18_999/18431_Just_Long_Neckties/18431_Just_Long_Neckties.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 11. 8
+이름 : 배성훈
+내용 : Just Long Neckties
+    문제번호 : 18431번
+
+    정렬, 그리디 문제다.
+    아이디어는 다음과 같다.
+    먼저 이상함이 최소가 되려면 현재 입을 넥타이와 n개의 선택할 넥타이들을 정렬하고
+    자기 순서에 맞는 넥타이를 고르면 이상함이 최소가 된다.
+
+    만약 ai를 현재 입은 넥타이 중 i번째로 작은거, bi를 선택할 넥타이 중 i번째로 작은거라 하자
+    그러면 a1 - b1, a2 - b2가 매칭되어야 하는데 a1 - b2, a2 - b1으로 매칭되었다면
+    a2 - b1은 a2 - b2보다 작으므로 고려할 대상이 아니다.
+    
+    반면 a1 - b2는 대소관계와 a1 <= a2, b1 <= b2이므로 a1 - b1 <= a1 - b2이고 동시에 a2 - b2 <= a1 - b2가 성립한다.
+    그래서 이상함이 증가한다.
+
+    이에 정렬한 뒤 순서대로 매칭해주는게 최대가 된다.
+    이제 정렬한 인덱스에 대해 상사가 i번째를 택하면 현재 사람들은 넥타이가 작은 순서대로 
+    자신의 선택에서 가장 작은 넥타이를 골라가면 된다.
+
+    해당 과정을 보면 왼쪽과 오른쪽으로 분할하면 반복됨을 알 수 있다.
+    만약 상사가 i번쨰를 고르면 1 ~ i - 2 번째는 바로 앞에서 사용한 결과와 같다.
+    오른쪽은 n + 1 ~ i 번째에서 i 경우만 뺀것과 같다.
+    이는 누적하는 아이디어를 이용하면 매번 범위를 탐색하는게 아닌 1칸씩 이동하면서 찾을 수 있다.
+    1 ~ i - 1번째의 최대값을 알고 잇을 때 1 ~ i 번째 최대값은 1 ~ i - 1의 최대값과 i번째 값을 비교하는 아이디어 말이다.
+
+    이렇게 누적합을 이용해 찾으니 이상없이 통과한다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1099
+    {
+
+        static void Main1099(string[] args)
+        {
+
+            int n;
+            (int val, int idx)[] select;
+            int[] cur, ret;
+
+            Solve();
+            void Solve()
+            {
+
+                Input();
+
+                GetRet();
+
+                Output();
+            }
+
+            void GetRet()
+            {
+
+                // 그리디로 i번째 넥타이를 택한 사람이 i번째로 넥타이를 선택하는게 이상함의 최소가 된다.
+                Array.Sort(select, (x, y) => x.val.CompareTo(y.val));
+                Array.Sort(cur);
+
+                int[] left = new int[n];
+                int[] right = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    // i번쨰 짧은 넥타이를 입은 사람이 i번째로 짧은 넥타이를 선택했을 때 이상함
+                    if (cur[i] < select[i].val) left[i] = select[i].val - cur[i];
+
+                    // i번째 짧은 넥타이를 입은 사람이 i + 1로 짧은 넥타이를 선택했을 때의 이상함
+                    if (cur[i] < select[i + 1].val) right[i] = select[i + 1].val - cur[i];
+                }
+
+                for (int i = 1; i < n; i++)
+                {
+                    
+                    // 왼쪽에 0 ~ i번째까지 이상함의 가장 큰 값을 찾아 저장
+                    left[i] = Math.Max(left[i - 1], left[i]);
+                    int j = n - 1 - i;
+                    // 오른쪽에 i ~ n 번째까지 이상함의 가장 큰 값을 찾아 저장
+                    right[j] = Math.Max(right[j], right[j + 1]);
+                }
+
+                for (int i = 0; i <= n; i++)
+                {
+
+                    // 상사가 선택하는 idx
+                    int idx = select[i].idx;
+                    // 왼쪽 이상함의 최대값
+                    int l = i == 0 ? 0 : left[i - 1];
+                    // 오른쪽 이상함의 최대값
+                    int r = i == n ? 0 : right[i];
+
+                    // 나머지 사원들의 이상함의 최대값
+                    ret[idx] = Math.Max(l, r);
+                }
+            }
+
+            void Output()
+            {
+
+                StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+                for (int i = 0; i <= n; i++)
+                {
+
+                    sw.Write($"{ret[i]} ");
+                }
+
+                sw.Close();
+            }
+
+            void Input()
+            {
+
+                StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+                n = ReadInt();
+                select = new (int val, int idx)[n + 1];
+                cur = new int[n];
+                ret = new int[n + 1];
+                for (int i = 0; i <= n; i++)
+                {
+
+                    select[i] = (ReadInt(), i);
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    cur[i] = ReadInt();
+                }
+
+                int ReadInt()
+                {
+
+                    int ret = 0;
+
+                    while (TryReadInt()) { }
+                    return ret;
+
+                    bool TryReadInt()
+                    {
+
+                        int c = sr.Read();
+
+                        if (c == '\r') c = sr.Read();
+                        if (c == '\n' || c == -1) return true;
+
+                        ret = c - '0';
+                        while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                        {
+
+                            if (c == '\r') continue;
+                            ret = ret * 10 + c - '0';
+                        }
+
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+#if other
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+using namespace std;
+typedef long long ll;
+
+int N;
+pair<int, int> P[200001];
+ll B[200002];
+ll L[200002], R[200002];
+ll ans[200001];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i <= N; i++) cin >> P[i].first, P[i].second = i;
+	sort(P, P + N + 1);
+
+	for (int i = 1; i <= N; i++) cin >> B[i];
+	sort(B + 1, B + N + 1);
+	for (int i = 1; i <= N; i++) {
+		L[i] = max((ll)P[i - 1].first - B[i], L[i - 1]);
+	}
+	for (int i = N; i > 0; i--) {
+		R[i] = max((ll)P[i].first - B[i], R[i + 1]);
+	}
+	for (int i = 0; i <= N; i++) {
+		ans[P[i].second] = max(L[i], R[i + 1]);
+	}
+
+	for (int i = 0; i <= N; i++) cout << ans[i] << ' ';
+}
+#endif
+}
---
+++ BaekJoon/10_000~19_999/18_000~18_999/18431_Just_Long_Neckties/README.md
@@ -0,0 +1,86 @@
+# C#
+
+## 난이도 : 골드 3
+
+## 알고리즘 분류
+  - 자료 구조
+  - 그리디 알고리즘
+  - 정렬
+  - 누적합
+
+## 제한조건
+  - 시간 제한 : 2초
+  - 메모리 제한 : 512 MB
+
+## 문제
+Have you ever heard of Just Odd Inventions, Ltd.? This company is known for their “just odd inventions.” We call it JOI, Ltd. in this problem. JOI, Ltd. has invented its newest product “Just Long Neckties”. There are N + 1 types of neckties, numbered 1 to N + 1. The length of the i-th necktie (1 ≤ i ≤ N + 1) is Ai.<br/>
+The company gathered their employees to hold a try-on party. N employees participate in the party, and the j-th employee (1 ≤ j ≤ N) initially wears a necktie of length Bj.<br/>
+The try-on party is held following this procedure:<br/>
+  1. CEO of JOI, Ltd. chooses a necktie, which is not used at the party.
+  2. Then, each employee chooses one of the remaining neckties to try on. No two employees choose the same necktie.
+  3. Finally, each employee takes off the necktie which (s)he initially wears and puts the selected necktie on.
+
+If an employee initially wearing a necktie of length b tries a necktie of length a, (s)he feels strangeness of max{a − b, 0}. The oddity of the try-on party is defined as the maximum strangeness among the employees.<br/>
+We also define Ck as the minimum oddity of the try-on party if CEO of JOI, Ltd. chooses the k-th necktie.<br/>
+Write a program which, given the lengths of the neckties used at the party and the neckties each employee initially wears, calculates the values of C1,C2, . . . ,CN+1.<br/>
+
+## 입력
+Read the following data from the standard input. Given values are all integers.<br/>
+
+	N
+	A1 . . . AN+1
+	B1 . . . BN
+
+## 출력
+Write one line to the standard output. The output should contain the values of C1,C2, . . . ,CN+1, separated by a space.<br/>
+
+## 제한
+  - 1 ≤ N ≤ 200 000.
+  - 1 ≤ Ai ≤ 1 000 000 000 (1 ≤ i ≤ N + 1).
+  - 1 ≤ Bj ≤ 1 000 000 000 (1 ≤ j ≤ N).
+
+## 예제 입력
+3<br/>
+4 3 7 6<br/>
+2 6 4<br/>
+
+## 예제 출력
+2 2 1 1<br/>
+
+## 힌트
+Here is an example of a try-on party:<br/>
+  - CEO of JOI, Ltd. chooses the 4th necktie. This necktie is not used in the party.
+  - The employee 1 chooses the 1st necktie, the employee 2 chooses the 2nd necktie, the employee 3 chooses the 3rd necktie.
+  - Each employee tries the necktie they choose on.
+
+In this case, strangeness of each employee is 2, 0, 3 in order. Therefore, the oddity of the party is 3. It is possible to decrease the oddity to 1 if the employees choose different neckties. One of the example is:<br/>
+  - CEO of JOI, Ltd. chooses the 4th necktie. This necktie is not used in the party.
+  - The employee 1 chooses the 2nd necktie, the employee 2 chooses the 3rd necktie, the employee 3 chooses the 1st necktie.
+  - Each employee tries the necktie they choose on.
+
+In this case, strangeness of each employee is 1, 1, 0 in order. Therefore, the oddity of the party is 1. This is the minimum possible oddity when CEO of JOI, Ltd. chooses the 4th necktie, so C4 = 1.<br/>
+
+## 풀이
+상사가 넥타이를 1개 먼저 선택하고 남은 n명의 사원이 n개의 넥타이를 선택할 때,<br/>
+이상함이 최소가 되는건 다음과 같다.<br/>
+ai를 i번째로 짧은 넥타이를 입은 넥타이 길이라하고, bi를 i번째로 짧은 선택할 넥타이 길이라 하자.<br/>
+그러면 ai 를 입은 사람이 bi를 택하는게 이상함이 가장 작다.<br/>
+만약 a1 - b2를 택했다고 가정하자 그러면 a1 <= a2이고, b1 <= b2가 자명하다.<br/>
+대소 관계로 a1 - b1 <= a1 - b2이고 동시에 a2 - b2 <= a1 - b2가 성립한다.<br/>
+이는 이상함의 최대값이 증가할 수도 있다.<br/>
+반면 a2 - b1 <= a1 - b1과 a2 - b2 <= a2 - b2이기에 최대값이 증가함에 영향을 안끼친다.<br/>
+이렇게 n명의 사원과 n개의 넥타이를 매칭시키면된다.<br/>
+
+
+또한 상사가 각 n에 대해 넥타이를 선택할 때 이상함의 최대값을 각각 찾아야한다.<br/>
+내림차순 정렬된 순서에 대해 생각하자 i 번째까지 택하고 i + 1 번째를 택한다고 보면,<br/>
+왼쪽은 1번에서 i - 1 번째까지의 최대값이고 여기에 i 번째를 추가해 비교하면 된다.<br/>
+
+
+오른쪽도 비슷하게 내림차순으로 정렬된 순서 i 번째를 택하고 i - 1번째를 보면,<br/>
+오른쪽은 n + 1번에서 i + 1번째까지의 최대값을 알고 i 번째를 추가해 비교하면 된다.<br/>
+이는 누적합 개념을 이용하면 상사가 선택에 따라 찾는 시간을 O(1)에 최대 이상함을 찾을 수 있게 한다.<br/>
+
+
+## 문제 링크
+https://www.acmicpc.net/problem/18110
