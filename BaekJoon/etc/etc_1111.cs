593c73de2e375e8fe69f1fa13f2a86f5e149ac23
TryingPop <qotjdgns2001@gmail.com>
2024-11-14 00:53:18 +0900

    docs: 최신화 2024. 11 etc 폴더 정리 및 풀이 추가

---
+++ BaekJoon/etc/etc_1111.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 11. -
+이름 : 배성훈
+내용 : 공정한 게임
+    문제번호 : 20370번
+
+
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1111
+    {
+    }
+}
