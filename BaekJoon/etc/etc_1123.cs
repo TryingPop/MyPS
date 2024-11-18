9d461f7caaa433ed0a47a126e677a1352c322c39
TryingPop <qotjdgns2001@gmail.com>
2024-11-18 21:58:31 +0900

    docs: 최신화 2024. 11 etc 폴더 정리

--- BaekJoon/etc/etc_1123.cs
+++ BaekJoon/etc/etc_1123.cs
@@ -4,18 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
-날짜 : 2024. 11. -
-이름 : 배성훈
-내용 : 공정한 게임
-    문제번호 : 20370번
-
-
-*/
-
 namespace BaekJoon.etc
 {
-    internal class etc_1111
+    internal class etc_1123
     {
     }
 }
