5e3d0382466106129378b5e98a71095ec6c68ac0
TryingPop <qotjdgns2001@gmail.com>
2024-12-18 17:12:53 +0900

    feat: 백준 연습용 문제 2024. 12 Study
    수학 문제다

---
+++ BaekJoon/30_000~39_999/30_000~30_999/30586_Ужин для интровертов/30586_Ужин для интровертов.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 12. 18
+이름 : 배성훈
+내용 : Ужин для интровертов
+    문제번호 : 30586번
+
+    직접 그려서 해보면 결과 = 2 * n / (k + 2)가 성립함을 알 수 있다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1200
+    {
+
+        static void Main1200(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int ret = 2 * n / (k + 2);
+            Console.Write(ret);
+        }
+    }
+}
