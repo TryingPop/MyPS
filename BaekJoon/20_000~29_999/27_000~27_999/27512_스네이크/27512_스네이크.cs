399b76d95abff9d89b0be36b044760ba0dfd5901
TryingPop <qotjdgns2001@gmail.com>
2025-02-20 02:23:25 +0900

    feat: 백준 연습용 문제 2025. 2 Study
    수학 문제다

---
+++ BaekJoon/20_000~29_999/27_000~27_999/27512_스네이크/27512_스네이크.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 12. 21
+이름 : 배성훈
+내용 : 스네이크
+    문제번호 : 27512번
+
+    수학, 애드 혹 문제다.
+    스네이크를 최대한 채우는 방법은 머리와 꼬리가 인접하는 것인데,
+    다른 문제를 풀면서 넓이가 홀수면 - 1, 짝수면 해당 넓이 값이 최대임을 알았다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1211
+    {
+
+        static void Main1211(string[] args)
+        {
+
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            int ret = input[0] * input[1];
+            if ((ret & 1) == 1) ret--;
+            Console.Write(ret);
+        }
+    }
+}
