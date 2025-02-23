c79d9ca5a6153dc92690fa1690982b14450045cf
TryingPop <qotjdgns2001@gmail.com>
2025-02-23 23:14:26 +0900

    feat: 백준 연습용 문제 2025. 2 Study
    구현 문제다

---
+++ BaekJoon/10_000~19_999/15_000~15_999/15917_노솔브_방지문제야!!/15917_노솔브_방지문제야!!.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 2. 22
+이름 : 배성훈
+내용 : 노솔브 방지문제야!!
+    문제번호 : 15917번
+
+    구현 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1355
+    {
+
+        static void Main1355(string[] args)
+        {
+
+            HashSet<int> corr = new(32);
+
+            for (int i = 0; i < 32; i++)
+            {
+
+                corr.Add(1 << i);
+            }
+            
+            using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+            using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+            int q = int.Parse(sr.ReadLine());
+
+            for (int i = 0; i < q; i++)
+            {
+
+                int cur = int.Parse(sr.ReadLine());
+                if (corr.Contains(cur)) sw.WriteLine(1);
+                else sw.WriteLine(0);
+            }
+        }
+    }
+}
