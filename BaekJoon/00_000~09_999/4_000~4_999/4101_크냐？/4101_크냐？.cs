ea3c8335beab6c02bc31c9b452a8dd548504d9da
TryingPop <qotjdgns2001@gmail.com>
2025-03-01 15:07:07 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    구현 문제다.

---
+++ BaekJoon/00_000~09_999/4_000~4_999/4101_크냐？/4101_크냐？.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 1 
+이름 : 배성훈
+내용 : 크냐?
+    문제번호: 4101번
+
+    단순 구현 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1368
+    {
+
+        static void Main1368(string[] args)
+        {
+
+            string Y = "Yes\n";
+            string N = "No\n";
+
+            using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+            using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+
+            int n, m;
+
+            while (Input())
+            {
+
+                sw.Write(n > m ? Y : N);
+            }
+
+            bool Input()
+            {
+
+                string[] temp = sr.ReadLine().Split();
+                n = int.Parse(temp[0]);
+                m = int.Parse(temp[1]);
+
+                return n != 0 || m != 0;
+            }
+        }
+    }
+}
