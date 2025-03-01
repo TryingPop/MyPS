f9e9663c3f06ffc3e53a0c85ed39f72e0e071e6c
TryingPop <qotjdgns2001@gmail.com>
2025-03-01 15:14:49 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    문자열 문제다.
    kmp를 쓰면 빠르게 찾을 수 있지만 여기서는 하나씩 찾았다.

---
+++ BaekJoon/10_000~19_999/16_000~16_999/16171_나는_친구가_적다_(Small)/16171_나는_친구가_적다_(Small).cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 2. 28
+이름 : 배성훈
+내용 : 나는 친구가 적다 (Small)
+    문제번호 : 16171번
+
+    문자열 문제다.
+    숫자부분을 지우고 해당 문자열이 있는지 찾아주면 된다.
+    여기서는 전체 문자열의 길이가 짧아 N^2의 방법으로 찾았다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1367
+    {
+
+        static void Main1367(string[] args)
+        {
+
+            string allStr = Console.ReadLine();
+            string findStr = Console.ReadLine();
+
+            Solve();
+
+            void Solve()
+            {
+
+                for (int i = 0; i < allStr.Length; i++)
+                {
+
+                    int match = 0;
+                    for (int j = i; j < allStr.Length; j++)
+                    {
+
+                        if ('0' <= allStr[j] && allStr[j] <= '9') continue;
+                        if (allStr[j] == findStr[match])
+                        {
+
+                            match++;
+                            if (match == findStr.Length)
+                            {
+
+                                Console.Write(1);
+                                return;
+                            }
+                            
+                            continue;
+                        }
+
+                        break;
+                    }
+                }
+
+                Console.Write(0);
+            }
+
+
+        }
+    }
+}
