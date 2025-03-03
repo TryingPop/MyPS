db44b0a85192517a2b272e6a916b99dbdecc9916
TryingPop <qotjdgns2001@gmail.com>
2025-03-03 17:17:26 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    문자열 문제다

---
+++ BaekJoon/00_000~09_999/4_000~4_999/4458_첫_글자를_대문자로/4458_첫_글자를_대문자로.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 2
+이름 : 배성훈
+내용 : 첫 글자를 대문자로
+    문제번호 : 4458번
+
+    구현, 문자열 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1370
+    {
+
+        static void Main1370(string[] args)
+        {
+
+            using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+            using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+
+            int n = int.Parse(sr.ReadLine());
+            for (int i =0; i < n; i++)
+            {
+
+                string input = sr.ReadLine();
+
+                if ('a' <= input[0] && input[0] <= 'z')
+                {
+
+                    int change = input[0] - 'a' + 'A';
+                    sw.Write((char)change);
+                }
+                else sw.Write(input[0]);
+
+                for (int j = 1; j < input.Length; j++)
+                {
+
+                    sw.Write(input[j]);
+                }
+
+                sw.Write('\n');
+            }
+        }
+    }
+}
---
+++ BaekJoon/00_000~09_999/4_000~4_999/4458_첫_글자를_대문자로/README.md
@@ -0,0 +1,46 @@
+# C#
+
+## 난이도 : 브론즈 3
+
+## 알고리즘 분류
+  - 구현
+  - 문자열
+
+## 제한조건
+  - 시간 제한 : 1초
+  - 메모리 제한 : 128 MB
+
+## 문제
+문장을 읽은 뒤, 줄의 첫 글자를 대문자로 바꾸는 프로그램을 작성하시오.<br/>
+
+
+## 입력
+첫째 줄에 줄의 수 N이 주어진다. 다음 N개의 줄에는 문장이 주어진다. 각 문장에 들어있는 글자의 수는 30을 넘지 않는다. 모든 줄의 첫 번째 글자는 알파벳이다.<br/>
+
+
+## 출력
+각 줄의 첫글자를 대문자로 바꾼뒤 출력한다.<br/>
+
+
+## 예제 입력
+5<br/>
+powdered Toast Man<br/>
+skeletor<br/>
+Electra Woman and Dyna Girl<br/>
+she-Ra Princess of Power<br/>
+darth Vader<br/>
+
+## 예제 출력
+Powdered Toast Man<br/>
+Skeletor<br/>
+Electra Woman and Dyna Girl<br/>
+She-Ra Princess of Power<br/>
+Darth Vader<br/>
+
+
+## 풀이
+줄단위로 문자열을 입력받고 첫문자가 소문자인 경우 대문자로 변형하면 된다.<br/>
+
+
+## 문제 링크
+https://www.acmicpc.net/problem/4458
