9fe4d6557667de5dca7d0380c0b96e52a9d6dbcc
TryingPop <qotjdgns2001@gmail.com>
2025-03-08 21:20:39 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    수학, dp, lis 문제다

---
+++ BaekJoon/etc/etc_1385.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 8
+이름 : 배성훈
+내용 : 가장 긴 감소하는 부분 수열
+    문제번호 : 11722번
+
+    lis 문제다.
+    가장 긴 감소하는 부분 수열은 뒤집어서 보면 가장 긴 증가하는 부분 수열과 같다.
+    그래서 뒤집고 lis 를 찾았다.
+    다만 이분 탐색에서 비교를 잘못해 2번 틀렸다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1385
+    {
+
+        static void Main1385(string[] args)
+        {
+
+            int n;
+            int[] arr;
+
+            Input();
+
+            GetRet();
+
+            void Reverse()
+            {
+
+                int i = 0;
+                int j = n - 1;
+                for (; i < j; i++, j--)
+                {
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            void GetRet()
+            {
+
+                Reverse();
+
+                int[] lis = new int[n];
+                int len = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    int idx = BinarySearch(arr[i]);
+                    lis[idx++] = arr[i];
+                    len = Math.Max(len, idx);
+                }
+
+                Console.Write(len);
+
+                int BinarySearch(int _num)
+                {
+
+                    int l = 0, r = len - 1;
+
+                    while (l <= r)
+                    {
+
+                        int mid = (l + r) >> 1;
+
+                        if (lis[mid] < _num) l = mid + 1;
+                        else r = mid - 1;
+                    }
+
+                    return l;
+                }
+            }
+
+            void Input()
+            {
+
+                using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+                n = int.Parse(sr.ReadLine());
+                arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            }
+        }
+    }
+}
---
+++ BaekJoon/etc/etc_1386.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 8
+이름 : 배성훈
+내용 : 이음줄
+    문제번호 : 14583번
+
+    수학 문제다.
+    사각형이 주어질 때, 대각선에 밑변이 겹치게 해야 한다.
+    접는 선은 밑변과 대각선의 각도를 반이 된다.
+
+    그러면 코사인 반각 공식으로 접는 선의 길이를 구할 수 있다.
+    접는 선과 밑변이 이루는 각을 x라 하면
+    cos 2x = 2 (cos x)^2 - 1을 얻고, cos 2x = 밑변 / 대각선 을 알 수 있다.
+    그래서 접는선 v를 구할 수 있다.
+    그러면 접는 선의 절반이 우리가 찾는 밑변이 된다.
+
+    이제 높이는 넓이를 이용해 구했다.
+    접는 선과 밑변이 이루는 삼각형에서 높이는 sin x이다.
+    sin x역시 코사인 반각 공식으로
+    cos 2x = 1 - 2 (sin x)^2을 얻고, sin x = 찾는 높이 / v 임을 알 수 있다.
+    앞의 v로 찾는 높이를 찾고, 해당 삼각형의 넓이를 양쪽으로 뺀 평행사변형의 넓이를 찾는다.
+
+    그러면 v x h = 밑변 x 찾는 높이 임을 알 수 있다.
+    이렇게 찾아 풀었다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1386
+    {
+
+        static void Main1386(string[] args)
+        {
+
+            double[] len = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+
+            double dia = Math.Sqrt(len[0] * len[0] + len[1] * len[1]);
+            double cos2x = len[0] / dia;
+
+            double cosx = Math.Sqrt((cos2x + 1.0) / 2.0);
+            double sinx = Math.Sqrt((1.0 - cos2x) / 2.0);
+
+            double v = len[0] / cosx;
+            double h = (len[1] - v * sinx) * len[0] / v;
+
+            v /= 2.0;
+            Console.Write($"{v:0.00} {h:0.00}");
+        }
+    }
+}
