31dbea7ca26dc9cb1e805ecc99b08ffae34feeb8
TryingPop <qotjdgns2001@gmail.com>
2025-03-14 04:57:27 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    사칙연산 문제다

---
+++ BaekJoon/20_000~29_999/20_000~20_999/20499_Darius님_한타_안_함？/20499_Darius님_한타_안_함？.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 10
+이름 : 배성훈
+내용 : Darius님 한타 안 함?
+    문제번호 : 20499번
+
+    수학, 사칙연산 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1392
+    {
+
+        static void Main1392(string[] args)
+        {
+
+            // 20499
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split('/'), int.Parse);
+            if (arr[1] == 0 || arr[0] + arr[2] < arr[1]) Console.Write("hasu");
+            else Console.Write("gosu");
+        }
+    }
+}
