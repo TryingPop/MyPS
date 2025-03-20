403dcfdd671215e407d2cfdaaac8127eba8546f2
TryingPop <qotjdgns2001@gmail.com>
2025-03-21 04:06:05 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    구현 문제다

---
+++ BaekJoon/00_000~09_999/2_000~2_999/2441_별_찍기_-_4/2441_별_찍기_-_4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 12. 28
+이름 : 배성훈
+내용 : 별 찍기 - 4
+    문제번호 : 2441번
+
+    구현 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1222
+    {
+
+        static void Main1222(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+
+            for (int i = n; i > 0; i--)
+            {
+
+                for (int j = 0; j < n - i; j++)
+                {
+
+                    sw.Write(' ');
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+
+                    sw.Write('*');
+                }
+
+                sw.Write('\n');
+            }
+        }
+    }
+}
