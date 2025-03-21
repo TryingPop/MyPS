6fb0e32e9bac251eacf7fda923b193e37a2f500c
TryingPop <qotjdgns2001@gmail.com>
2025-03-22 01:16:45 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    피타고라스 정리 문제다

---
+++ BaekJoon/00_000~09_999/1_000~1_999/1297_TV_크기/1297_TV_크기.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 21
+이름 : 배성훈
+내용 : TV 크기
+    문제번호 : 1297번
+
+    기하학, 피타고라스 정리 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1435
+    {
+
+        static void Main1435(string[] args)
+        {
+
+            string[] temp = Console.ReadLine().Split();
+            int D = int.Parse(temp[0]);
+            int W = int.Parse(temp[1]);
+            int H = int.Parse(temp[2]);
+
+            int PythaD = W * W + H * H;
+            int PowD = D * D;
+            double MUL = Math.Sqrt(PowD / (1.0 * PythaD));
+            int w = (int)(W * MUL);
+            int h = (int)(H * MUL);
+
+            Console.Write($"{w} {h}");
+        }
+    }
+}
