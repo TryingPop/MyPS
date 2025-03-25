d077c95fb83126574620e7137f66963b42196fc7
TryingPop <qotjdgns2001@gmail.com>
2025-03-25 20:34:42 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    구현 문제다

---
+++ BaekJoon/00_000~09_999/1_000~1_999/1_100~1_199/1159_농구_경기/1159_농구_경기.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 25
+이름 : 배성훈
+내용 : 농구 경기
+    문제번호 : 1159번
+
+    문자열 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1465
+    {
+
+        static void Main1465(string[] args)
+        {
+
+            using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+            int n = int.Parse(sr.ReadLine());
+
+            int[] cnt = new int[255];
+            for (int i = 0; i < n; i++)
+            {
+
+                string input = sr.ReadLine();
+                cnt[input[0]]++;
+            }
+            using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+
+            bool flag = true;
+            for (int i = 0; i < 255; i++)
+            {
+
+                if (cnt[i] < 5) continue;
+                sw.Write((char)i);
+                flag = false;
+            }
+
+            if (flag) sw.Write("PREDAJA");
+        }
+    }
+}
