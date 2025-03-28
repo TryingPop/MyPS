2fafc06259b0679b0f581c17b1c0ec1b776f079a
TryingPop <qotjdgns2001@gmail.com>
2025-03-29 00:40:55 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    사칙연산 문제다.

---
+++ BaekJoon/00_000~09_999/1_000~1_999/1_200~1_299/1252_이진수_덧셈/1252_이진수_덧셈.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 27
+이름 : 배성훈
+내용 : 이진수 덧셈
+    문제번호 : 1252번
+
+    사칙연산 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1484
+    {
+
+        static void Main1484(string[] args)
+        {
+
+            string[] temp = Console.ReadLine().Split();
+            int[] arr = new int[82];
+
+            for (int i = temp[0].Length - 1, j = 0; i >= 0; i--, j++)
+            {
+
+                arr[j] = temp[0][i] - '0';
+            }
+
+            for (int i = temp[1].Length - 1, j = 0; i >= 0; i--, j++)
+            {
+
+                arr[j] += temp[1][i] - '0';
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+
+                while (arr[i] > 1)
+                {
+
+                    arr[i] -= 2;
+                    arr[i + 1]++;
+                }
+            }
+
+            int e = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] == 1) e = i;
+            }
+
+            for (int i = e; i >= 0; i--)
+            {
+
+                Console.Write(arr[i]);
+            }
+        }
+    }
+}
