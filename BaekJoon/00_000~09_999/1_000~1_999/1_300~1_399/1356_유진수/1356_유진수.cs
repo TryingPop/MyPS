f73d3886aa861edac106a9eff2eee805c7ce5c51
TryingPop <qotjdgns2001@gmail.com>
2025-03-30 05:08:43 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    구현, 문자열, 누적합 문제다

---
+++ BaekJoon/00_000~09_999/1_000~1_999/1_300~1_399/1356_유진수/1356_유진수.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 29
+이름 : 배성훈
+내용 : 유진수
+    문제번호 : 1356번
+
+    사칙연산 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1490
+    {
+
+        static void Main1490(string[] args)
+        {
+
+            string temp = Console.ReadLine();
+
+            int[] right = new int[temp.Length];
+            right[temp.Length - 1] = temp[temp.Length - 1] - '0';
+            for (int i = right.Length - 2; i >= 0; i--)
+            {
+
+                right[i] = right[i + 1] * (temp[i] - '0');
+            }
+
+            bool ret = false;
+            int left = 1;
+            for (int i = 0; i < temp.Length - 1; i++)
+            {
+
+                left *= temp[i] - '0';
+                if (left != right[i + 1]) continue;
+                ret = true;
+                break;
+            }
+
+            Console.Write(ret ? "YES" : "NO");
+        }
+    }
+}
