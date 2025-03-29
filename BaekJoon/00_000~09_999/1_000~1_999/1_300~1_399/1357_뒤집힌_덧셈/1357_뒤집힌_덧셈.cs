573d7e25d7cb2146fc09a9b2550bff1169d5d386
TryingPop <qotjdgns2001@gmail.com>
2025-03-30 05:13:05 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    구현, 문자열 문제다.
    문자열을 뒤집어서 숫자로 변환하는 함수를 만들면 해결된다

---
+++ BaekJoon/00_000~09_999/1_000~1_999/1_300~1_399/1357_뒤집힌_덧셈/1357_뒤집힌_덧셈.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 29
+이름 : 배성훈
+내용 : 뒤집힌 덧셈
+    문제번호 : 1357번
+
+    문자열 문제다.
+    345 5 반례를 캐치 못해 한 번 틀렸다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1491
+    {
+
+        static void Main1491(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split();
+
+            int a = Rev(input[0]);
+            int b = Rev(input[1]);
+            string rev = (a + b).ToString();
+            int ret = Rev(rev);
+
+            Console.Write(ret);
+
+            int Rev(string _str)
+            {
+
+                int ret = 0;
+                for (int i = _str.Length - 1; i >= 0; i--)
+                {
+
+                    ret = ret * 10 + _str[i] - '0';
+                }
+
+                return ret;
+            }
+        }
+    }
+}
