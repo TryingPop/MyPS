3154cc3406d4e166b59ad76fa4a3af761e0650a3
TryingPop <qotjdgns2001@gmail.com>
2025-03-30 05:36:03 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    dp 문제다

---
+++ BaekJoon/00_000~09_999/1_000~1_999/1_300~1_399/1354_무한_수열_2/1354_무한_수열_2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 29
+이름 : 배성훈
+내용 : 무한 수열 2
+    문제번호 : 1354번
+
+    dp, 해시 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1492
+    {
+
+        static void Main1492(string[] args)
+        {
+
+            Dictionary<long, long> dp = new(100_000);
+            // 15초 걸린다;
+            // SortedDictionary<long, long> dp = new();
+            long n, p, q, x, y;
+
+            Input();
+
+            Console.Write(DFS(n));
+
+            void Input()
+            {
+
+                string[] temp = Console.ReadLine().Split();
+                n = long.Parse(temp[0]);
+                p = long.Parse(temp[1]);
+                q = long.Parse(temp[2]);
+                x = long.Parse(temp[3]);
+                y = long.Parse(temp[4]);
+            }
+
+            long DFS(long _cur)
+            {
+
+                if (_cur <= 0) return 1;
+
+                if (dp.ContainsKey(_cur)) return dp[_cur];
+                dp[_cur] = DFS((_cur / p) - x) + DFS((_cur / q) - y);
+
+                return dp[_cur];
+            }
+        }
+    }
+}
