14686bea290a55e62e16111827e09dc70e61adec
TryingPop <qotjdgns2001@gmail.com>
2025-03-31 20:50:22 +0900

    feat: 백준 연습용 문제 2025. 3 Study
    구현, 수학, 트리, 세그먼트 트리 문제다

--- BaekJoon/etc/etc_1498.cs
+++ BaekJoon/etc/etc_1498.cs
@@ -4,9 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+날짜 : 2025. 3. 30
+이름 : 배성훈
+내용 : 펫
+    문제번호 : 1362번
+
+    시뮬레이션 문제다.
+*/
+
 namespace BaekJoon.etc
 {
     internal class etc_1498
     {
+
+        static void Main1362(string[] args)
+        {
+
+            string H = ":-)\n";
+            string S = ":-(\n";
+            string R = "RIP\n";
+
+            char F = 'F';
+            char E = 'E';
+            char LAST = '#';
+
+            using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+            using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+
+            int o, w;
+            int idx = 1;
+            while (Input())
+            {
+
+                GetRet();
+            }
+
+            void GetRet()
+            {
+
+                string input;
+                while ((input = sr.ReadLine())[0] != LAST)
+                {
+
+                    if (w == 0) continue;
+                    int val = int.Parse(input.Split()[1]);
+                    if (input[0] == F)
+                        w += val;
+                    else
+                    {
+
+                        w -= val;
+                        if (w < 0) w = 0;
+                    }
+                }
+
+                sw.Write(idx++);
+                sw.Write(' ');
+                if (w == 0) sw.Write(R);
+                else if (w < o * 2 && w > o / 2) sw.Write(H);
+                else sw.Write(S);
+            }
+
+            bool Input()
+            {
+
+                string[] temp = sr.ReadLine().Split();
+                o = int.Parse(temp[0]);
+                w = int.Parse(temp[1]);
+
+                return o != 0 && w != 0;
+            }
+        }
     }
 }
---
+++ BaekJoon/etc/etc_1499.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 3. 30
+이름 : 배성훈
+내용 : 노래 악보
+    구현 문제다.
+
+    누적합과 이분탐색을 이용해 풀었다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1499
+    {
+
+        static void Main1499(string[] args)
+        {
+
+            using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+            using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+            int n, m;
+            int[] arr;
+
+            Input();
+
+            GetRet();
+
+            void GetRet()
+            {
+
+                for (int i = 0; i < m; i++)
+                {
+
+                    int time = int.Parse(sr.ReadLine());
+
+                    sw.Write(BinarySearch(time));
+                    sw.Write('\n');
+                }
+
+                int BinarySearch(int _val)
+                {
+
+                    int l = 1;
+                    int r = n;
+
+                    while (l <= r)
+                    {
+
+                        int mid = (l + r) >> 1;
+
+                        // _val 초 이상이되면 다음 l은 다음 인덱스를 가리킨다.
+                        if (arr[mid] <= _val) l = mid + 1;
+                        else r = mid - 1;
+                    }
+
+                    return l;
+                }
+            }
+
+            void Input()
+            {
+
+                string[] temp = sr.ReadLine().Split();
+                n = int.Parse(temp[0]);
+                m = int.Parse(temp[1]);
+                arr = new int[n + 1];
+                for (int i = 1; i <= n; i++)
+                {
+
+                    arr[i] = int.Parse(sr.ReadLine());
+                }
+
+                for (int i = 2; i <= n; i++)
+                {
+
+                    arr[i] += arr[i - 1];
+                }
+            }
+        }
+    }
+}
