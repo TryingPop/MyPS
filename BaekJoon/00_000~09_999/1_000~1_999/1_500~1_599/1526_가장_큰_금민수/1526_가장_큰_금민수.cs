51a1c7544d9074be45a0bfc19299b879ae29ca59
TryingPop <qotjdgns2001@gmail.com>
2025-04-07 21:37:35 +0900

    feat: 백준 연습용 문제 2025. 4 Study
    구현 문제다

---
+++ BaekJoon/00_000~09_999/1_000~1_999/1_500~1_599/1526_가장_큰_금민수/1526_가장_큰_금민수.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 4. 7
+이름 : 배성훈
+내용 : 가장 큰 금민수
+    문제번호 : 1526번
+
+    구현, 브루트포스 문제다.
+    DFS를 이용해 해결했다.
+*/
+namespace BaekJoon.etc
+{
+    internal class etc_1526
+    {
+
+        static void Main1526(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int ret = 0;
+            DFS();
+            Console.Write(ret);
+            void DFS(int _cur = 0)
+            {
+
+                if (_cur > n) return;
+
+                ret = Math.Max(ret, _cur);
+                DFS(_cur * 10 + 4);
+                DFS(_cur * 10 + 7);
+            }
+        }
+    }
+}
