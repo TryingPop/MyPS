dfaa73b39a1d0e423994d04d81502fe85802654e
TryingPop <qotjdgns2001@gmail.com>
2025-04-09 17:02:28 +0900

    feat: 백준 연습용 문제 2025. 4 Study
    구현, 시뮬레이션 문제다

---
+++ BaekJoon/20_000~29_999/24_000~24_999/24049_정원_(Easy)/24049_정원_(Easy).cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 15
+이름 : 배성훈
+내용 : 정원 (Easy)
+    문제번호 : 24049번
+
+    간단한 구현 문제다
+    주어진 조건대로 구현했다
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0232
+    {
+
+        static void Main232(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+
+            int row = ReadInt(sr);
+            int col = ReadInt(sr);
+
+            int[,] board = new int[row + 1, col + 1];
+
+            {
+
+                for (int i = 1; i <= row; i++)
+                {
+
+                    board[i, 0] = ReadInt(sr);
+                }
+
+                for (int j = 1; j <= col; j++)
+                {
+
+                    board[0, j] = ReadInt(sr);
+                }
+            }
+            sr.Close();
+
+
+            for (int r = 1; r <= row; r++)
+            {
+
+                for (int c = 1; c <= col; c++)
+                {
+
+                    if (board[r - 1, c] == board[r, c - 1]) board[r, c] = 0;
+                    else board[r, c] = 1;
+                }
+            }
+
+            Console.WriteLine(board[row, col]);
+        }
+
+        static int ReadInt(StreamReader _sr)
+        {
+
+            int c, ret = 0;
+
+            while((c = _sr.Read()) != -1 && c != ' ' && c != '\n')
+            {
+
+                if (c == '\r') continue;
+                ret = ret * 10 + c - '0';
+            }
+
+            return ret;
+        }
+    }
+}
