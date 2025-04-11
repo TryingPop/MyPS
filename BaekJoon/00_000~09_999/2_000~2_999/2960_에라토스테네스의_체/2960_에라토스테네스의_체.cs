4eb4d507858a78072b4ffe21425e4ef50893a8aa
TryingPop <qotjdgns2001@gmail.com>
2025-04-11 23:22:41 +0900

    feat: 백준 연습용 문제 2025. 4 Study
    에라토스테네스의 체를 구현하는 문제다

---
+++ BaekJoon/00_000~09_999/2_000~2_999/2960_에라토스테네스의_체/2960_에라토스테네스의_체.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 4. 11
+이름 : 배성훈
+내용 : 에라토스테네스의 체
+    문제번호 : 2960번
+
+    에라토스테네스의 체 구현 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1535
+    {
+
+        static void Main1535(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split();
+            bool[] arr = new bool[int.Parse(input[0]) + 1];
+            int cnt = int.Parse(input[1]);
+
+            for (int i = 2; i < arr.Length && 0 < cnt; i++)
+            {
+
+                if (arr[i]) continue;
+
+                for (int j = i; j < arr.Length; j += i)
+                {
+
+                    if (arr[j]) continue;
+                    arr[j] = true;
+                    cnt--;
+                    if (cnt == 0)
+                    {
+
+                        Console.Write(j);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
