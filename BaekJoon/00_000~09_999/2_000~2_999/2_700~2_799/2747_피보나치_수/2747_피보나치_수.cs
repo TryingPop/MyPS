018e12f3a4f8283ac366ab9c0e45d3d4ccfd5bc1
TryingPop <qotjdgns2001@gmail.com>
2025-04-19 19:54:20 +0900

    feat: 백준 연습용 문제 2025. 4 Study
    수학 문제다.
    Fibonacci 45항은 ulong 범위로 표현된다.
    그래서 ulong 자료형과 for 문으로 피보나치 점화식을 실행해 찾아주면 된다.

---
+++ BaekJoon/00_000~09_999/2_000~2_999/2_700~2_799/2747_피보나치_수/2747_피보나치_수.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 4. 18
+이름 : 배성훈
+내용 : 피보나치 수
+    문제번호 : 2747번
+
+    수학 문제다.
+    45항까지는 ulong 범위로 표현할 수 있다.
+    입력되는 범위가 1항 이상이므로 따로 0항은 반례처리가 필요 없다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1554
+    {
+
+        static void Main1554(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            ulong prev = 0, cur = 1;
+            // n > 0
+            for (int i = 1; i < n; i++)
+            {
+
+                ulong temp = cur;
+                cur = prev + cur;
+                prev = temp;
+            }
+
+            ulong ret = cur;
+
+            Console.Write(ret);
+        }
+    }
+}
