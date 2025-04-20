63d9aa9d976bc636049b6b2a84b1d1ccb2bf6e6e
TryingPop <qotjdgns2001@gmail.com>
2025-04-20 17:13:19 +0900

    feat: 백준 연습용 문제 2025. 4 Study
    수학, 게임 이론 문제다.

---
+++ BaekJoon/20_000~29_999/20_000~20_999/20004_베스킨라빈스_31/20004_베스킨라빈스_31.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2024. 3. 18
+이름 : 배성훈
+내용 : 베스킨라빈스 31
+    문제번호 : 20004번
+
+    수학, 게임이론 문제다
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_0278
+    {
+
+        static void Main278(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            bool[] ret = new bool[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (31 % (i + 1) == 1) ret[i] = true;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (ret[i]) Console.Write($"{i}\n");
+            }
+        }
+    }
+}
