3a495f627fa06b530288ffcfd127c7767e414522
TryingPop <qotjdgns2001@gmail.com>
2025-04-30 20:10:21 +0900

    feat: 백준 연습용 문제 2025. 4 Study
    dp, 정렬 문제다.

---
+++ BaekJoon/00_000~09_999/6_000~6_999/6136_Milking_Time/6136_Milking_Time.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 4. 29
+이름 : 배성훈
+내용 : Milking Time
+    문제번호 : 6136번
+
+    dp문제다.
+    M의 크기가 1000으로 작다.
+    M^2의 방법이 유효하다.
+    이전 최대 시간을 모두 저장한다.
+
+    아이디어는 다음과 같다.
+    먼저 시작 시간을 기준으로 정렬한다.
+
+    순차적으로 일을 시도하는데 가능한 것 중 가장 효율이 좋은 경우를 찾는다.
+    첫 일의 경우 0초에 0효율이다.
+    그러면 해당 일을 하는 경우 arr[0].e + r에 일이 가능하고, 효율을 arr[i].v이다.
+    이를 chk 배열에 시간과 효율을 넣는다.
+    
+    두 번째 일의 경우 chk[0]의 시간에 현재 일을 가능한 경우
+    해당 시간으로 일을한 효율을 택한다.
+    반면 불가능하다면 0, 0초에서 한다고 본다.
+    그리고 arr[1].e + r시간과 찾은 효율 + arr[i].v를 해서 chk 배열에 넣는다.
+    이렇게 계속해서 일을해간다.
+
+    그리고 마지막에 chk배열 중 가장 높은 효율을 찾으면 된다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1589
+    {
+
+        static void Main1589(string[] args)
+        {
+
+            int n, m, r;
+            (int s, int e, int v)[] arr;
+
+            Input();
+
+            GetRet();
+
+            void GetRet()
+            {
+
+                // 시작 시간으로 정렬한다.
+                Array.Sort(arr, (x, y) => x.s.CompareTo(y.s));
+
+                // time : 이전에 일을 마치고 쉬었던 시간
+                // val : 해당 일을 할 때 최고 효율
+                (int time, int val)[] chk = new (int time, int val)[m];
+                int len = 0;
+
+                for (int i = 0; i < m; i++)
+                {
+
+                    int curStart = arr[i].s;
+
+                    // 초기 0효율
+                    int v = 0;
+                    for (int j = 0; j < len; j++)
+                    {
+
+                        // 이전 마치고 휴식한 시간이 현재일을 못하는 경우 패스
+                        if (curStart < chk[j].time) continue;
+
+                        // 일을 할 수 있는 경우 최대 효율을 찾는다.
+                        if (v < chk[j].val)
+                            v = chk[j].val;
+                    }
+
+                    // 찾은 최대 효율에서 현재 일을 한 효율을 chk에 넣는다.
+                    chk[len++] = (arr[i].e + r, v + arr[i].v);
+                }
+
+                int ret = 0;
+                for (int i = 0; i < len; i++)
+                {
+
+                    // 최고 효율 찾기
+                    ret = Math.Max(chk[i].val, ret);
+                }
+
+                Console.Write(ret);
+            }
+
+            void Input()
+            {
+
+                using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+
+                n = ReadInt();
+                m = ReadInt();
+                r = ReadInt();
+
+                arr = new (int s, int e, int v)[m];
+                for (int i = 0; i < m; i++)
+                {
+
+                    arr[i] = (ReadInt(), ReadInt(), ReadInt());
+                }
+
+                int ReadInt()
+                {
+
+                    int ret = 0;
+
+                    while (TryReadInt()) ;
+                    return ret;
+
+                    bool TryReadInt()
+                    {
+
+                        int c = sr.Read();
+                        if (c == '\r') c = sr.Read();
+                        if (c == '\n' || c == ' ') return true;
+
+                        ret = c - '0';
+
+                        while((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                        {
+
+                            if (c == '\r') continue;
+                            ret = ret * 10 + c - '0';
+                        }
+
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+#if other
+// #include<stdio.h>
+int time[1001][3], Tcnt[1001];
+int push(int n, int end);
+int main(){
+	int n, m, r, i, j, k, a, b, c, cnt=0, max=-999, allMax=-999, lastTime;
+	scanf("%d %d %d", &n, &m, &r);
+	for(i=1; i<=m; i++){
+		scanf("%d %d %d", &a, &b, &c);
+		j = i;
+		do{
+			j--;
+			if(j == 0)
+				break;
+		}while(time[j][0] > a);
+		push(j+1, i);
+		time[j+1][0] = a;
+		time[j+1][1] = b;
+		time[j+1][2] = c;
+	}
+	for(i=m; i>=1; i--){
+		cnt=0;
+		max = 0;
+		for(j=i+1; j<=m; j++){
+			if(time[j][0] < time[i][1] + r)
+				continue;
+			if(Tcnt[j] > max)
+				max = Tcnt[j];
+			if(Tcnt[j] == allMax)
+				break;
+		}
+		Tcnt[i] = max+time[i][2];
+		if(allMax < Tcnt[i])
+			allMax = Tcnt[i];
+	}
+	printf("%d", allMax);
+	return 0;
+}
+int push(int n, int end){
+	if(n < end)
+		push(n+1, end);
+	time[n+1][0] = time[n][0];
+	time[n+1][1] = time[n][1];
+	time[n+1][2] = time[n][2];
+	return 0;
+}
+#endif
+}
---
+++ BaekJoon/00_000~09_999/6_000~6_999/6136_Milking_Time/README.md
@@ -0,0 +1,54 @@
+# C#
+
+## 난이도 : 골드 4
+
+## 알고리즘 분류
+  - 다이나믹 프로그래밍
+  - 정렬
+
+## 제한조건
+  - 시간 제한 : 1초
+  - 메모리 제한 : 128 MB
+
+## 문제
+Bessie is such a hard-working cow. In fact, she is so focused on maximizing her productivity that she decides to schedule her next N (1 <= N <= 1,000,000) hours (conveniently labeled 0..N-1) so that she produces as much milk as possible.<br/>
+Farmer John has a list of M (1 <= M <= 1,000) possibly overlapping intervals in which he is available for milking. Each interval i has a starting hour (0 <= starting_hour_i < N), an ending hour (starting_hour_i < ending_hour_i <= N), and a corresponding efficiency (1 <= efficiency_i <= 1,000,000) which indicates how many gallons of milk that he can get out of Bessie in that interval. Farmer John starts and stops milking at the beginning of the starting hour and ending hour, respectively. When being milked, Bessie must be milked through an entire interval.<br/>
+Even Bessie has her limitations, though. After being milked during any interval, she must rest R (1 <= R <= N) hours before she can start milking again. Given Farmer Johns list of intervals, determine the maximum amount of milk that Bessie can produce in the N hours.<br/>
+
+
+## 입력
+  - Line 1: Three space-separated integers: N, M, and R
+  - Lines 2..M+1: Line i+1 describes FJ's ith milking interval with three space-separated integers: starting_hour_i, ending_hour_i, and efficiency_i
+
+
+## 출력
+  - Line 1: The maximum number of gallons of milk that Bessie can product in the N hours
+
+
+## 예제 입력
+12 4 2<br/>
+1 2 8<br/>
+10 12 19<br/>
+3 6 24<br/>
+7 10 31<br/>
+
+
+## 예제 출력
+43<br/>
+
+
+## 풀이
+시간은 100만까지 들어오고, M은 1000으로 1초 단위로 접근하면 시간초과 난다.<br/>
+그런데 변화가 오는 시간만 나타내면 3M으로 줄일 수 있다.<br/>
+시작 시간, 끝 시간, 끝나고 휴식이 끝난 시간 이렇게 3개로 구분 가능하다.<br/>
+여기서 끝 시간을 휴식 시간까지 포함하면 2M으로 줄일 수 있다.<br/>
+
+
+그래서 해당 특정 시간에 대해 배낭 비슷하게 접근해도 된다.<br/>
+혹은 그리디로 시작 시간을 기준으로 정렬한다.<br/>
+그리고 이전 가능한 최대 경우에 대해 해당 일을 시켜 최댓값을 찾아도 된다.<br/>
+여기서는 그리디로 접근했다.<br/>
+
+
+## 문제 링크
+https://www.acmicpc.net/problem/6136
