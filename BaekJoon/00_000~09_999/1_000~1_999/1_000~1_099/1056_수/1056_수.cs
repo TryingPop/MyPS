ace19cabf93ee6c8d2875034664559b6078e447f
TryingPop <qotjdgns2001@gmail.com>
2025-05-06 15:38:15 +0900

    fix: 파일 수정 2025. 5 Study
    코드 잘못 올려서 수정

--- BaekJoon/00_000~09_999/1_000~1_999/1_000~1_099/1056_수/1056_수.cs
+++ BaekJoon/00_000~09_999/1_000~1_999/1_000~1_099/1056_수/1056_수.cs
@@ -1,124 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 /*
 날짜 : 2025. 5. 6
 이름 : 배성훈
-내용 : 밥
-    문제번호 : 23559번
+내용 : 수
+    문제번호 : 1056번
 
-    그리디, 정렬 문제다.
-    5000원짜리 맛 - 1000원짜리 맛의 값이 큰 것을 바꾸는게 전체 총합이 늘어난다.
-    그래서 5000원짜리를 살 수 있는 최대갯수를 x라 하면 5000원짜리 맛 - 1000원짜리 맛이 큰 것 x개를 사는게 좋다.
-    5000원짜리 맛 - 1000원짜리 맛이 음수인 경우 5000원짜리를 안사는게 좋다.
-    그래서 0으로 반례처리를 하거나 0인 경우 탈출을 하면 된다.
+    dp, 이분 탐색 문제다.
+    아이디어는 다음과 같다.
+    dp[i]를 1에서 i로 이동하는 최소 경우라 하자.
+    그러면 i < j 에대해 dp[j] - dp[i] ≤ j - i임을 알 수 있다.
+    1씩 증가하는 경로로 가면 i로 가는 경우 i - 1이고 하나의 경로이다.
+    dp의 정의가 최단 경로이므로 dp[i] < i가 성립한다.
+
+    i 에서 j로 1번으로 이동하면 하나의 경로가 되고 dp[j] ≤ dp[i] + j - i이다.
+    그래서 dp[j] - dp[i] ≤ j - i이다.
+
+    그래서 각 x에 대해 a를 a^x ≤ n인 최댓값이라 하자.
+    그러면 b < a 인 경우 b^x로 n으로 이동하는 것은 a^x로 이동하는 것보다 거리가 큼을 알 수있다.
+    마찬가지로 a + 1 < b에 대해 b^x로 n으로 이동하는 것은 a + 1보다 큼을 알 수 있다.
+    그래서 a와, a + 1로 이동한 경우만 비교하면 된다.
+
+    모든 x에 대해 a를 찾고 a,  a+ 1로 이동한 최단 경로를 찾아주면 된다.
 */
 
 namespace BaekJoon.etc
 {
-    internal class etc_1616
+    internal class etc_1615
     {
 
-        static void Main1616(string[] args)
+        static void Main1615(string[] args)
         {
 
-            int n, x;
-            int[] arr;
-            int ret;
+            long INF = 4_000_000_000_000_000_000;
 
-            Input();
+            int MAX = 60;
+            long n = Convert.ToInt64(Console.ReadLine());
 
-            GetRet();
+            Dictionary<long, long> dp = new(300_000);
+            dp[1] = 0;
+            dp[0] = 1;
+            Console.Write(DFS(n));
 
-            void GetRet()
+            long DFS(long _dep)
             {
 
-                Array.Sort(arr, (x, y) => y.CompareTo(x));
-                for (int i = 0; i < x; i++)
+                if (dp.ContainsKey(_dep)) return dp[_dep];
+                dp[_dep] = _dep - 1;
+                long ret = dp[_dep];
+
+                for (int i = 2; i <= MAX; i++)
                 {
 
-                    if (arr[i] <= 0) break;
-                    ret += arr[i];
-                }
+                    // a^i < _dpe인 가장 큰 a를 이분 탐색으로 찾는다.
+                    // 이는 _dep와 가까운 a^i를 찾기 위해서이다.
+                    long a = BinarySearch(_dep, i);
 
-                Console.Write(ret);
-            }
-
-            void Input()
-            {
-
-                using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
-
-                n = ReadInt();
-                x = ((int)(ReadLong() / 1_000) - n) / 4;
-
-                arr = new int[n];
-                ret = 0;
-                for (int i = 0; i < n; i++)
-                {
-
-                    int b = ReadInt();
-                    int a = ReadInt();
-
-                    arr[i] = b - a;
-                    ret += a;
-                }
-
-                long ReadLong()
-                {
-
-                    long ret = 0;
-
-                    while (TryReadLong()) ;
-                    return ret;
-
-                    bool TryReadLong()
+                    for (int j = 0; j < 2; j++, a++)
                     {
 
-                        int c = sr.Read();
-                        if (c == '\r') c = sr.Read();
-                        if (c == '\n' || c == ' ') return true;
-                        ret = c - '0';
-
-                        while ((c = sr.Read()) != -1 && c != ' ' && c != '\n')
-                        {
-
-                            if (c == '\r') continue;
-                            ret = ret * 10 + c - '0';
-                        }
-
-                        return false;
+                        long cur = DFS(a);
+                        long add = Math.Abs(MyPow(a, i) - _dep);
+                        ret = Math.Min(ret, cur + 1 + add);
                     }
                 }
 
-                int ReadInt()
+                return dp[_dep] = ret;
+
+                long MyPow(long _a, int _exp)
                 {
 
-                    int ret = 0;
-
-                    while (TryReadInt()) ;
-                    return ret;
-
-                    bool TryReadInt()
+                    bool flag = false;
+                    long ret = 1;
+                    while (_exp > 0)
                     {
 
-                        int c = sr.Read();
-                        if (c == '\r') c = sr.Read();
-                        if (c == '\n' || c == ' ') return true;
-                        ret = c - '0';
+                        if ((_exp & 1) == 1) ret = MyMul(ret, _a);
+                        _exp >>= 1;
+                        _a = MyMul(_a, _a);
+                        if (flag) break;
+                    }
 
-                        while ((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                    if (flag) return INF;
+                    else return ret;
+
+                    // 곱셈 정의
+                    long MyMul(long _f, long _t)
+                    {
+
+                        long ret = 0;
+                        while (_t > 0)
                         {
 
-                            if (c == '\r') continue;
-                            ret = ret * 10 + c - '0';
+                            if ((_t & 1L) == 1L) ret += _f;
+                            _t >>= 1;
+                            _f += _f;
+                            if (_f > INF)
+                                return INF;
                         }
 
-                        return false;
+                        return ret;
                     }
+                }
+
+                long BinarySearch(long _chk, int _exp)
+                {
+
+                    long l = 1;
+                    long r = 1_000_000_000;
+
+                    while (l <= r)
+                    {
+
+                        long mid = (l + r) >> 1;
+                        long pow = MyPow(mid, _exp);
+
+                        if (pow <= _chk) l = mid + 1;
+                        else r = mid - 1;
+                    }
+
+                    return l - 1;
                 }
             }
         }
