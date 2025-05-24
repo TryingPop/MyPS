383950a4f6c6f476545d1c5a7e2c2187070a53ea
TryingPop <qotjdgns2001@gmail.com>
2025-05-24 21:42:18 +0900

    feat: 백준 연습용 문제 2025. 5 Study
    사칙연산 문제다.

---
+++ BaekJoon/30_000~39_999/33_000~33_999/33165_徒競走_(Footrace)/33165_徒競走_(Footrace).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 5. 22
+이름 : 배성훈
+내용 : 徒競走 (Footrace)
+    문제번호 : 33165번
+
+    사칙연산 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1637
+    {
+
+        static void Main1637(string[] args)
+        {
+
+            int t = int.Parse(Console.ReadLine());
+            int v = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(t * v);
+        }
+    }
+}
