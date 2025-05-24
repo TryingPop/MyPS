ea325174073d89862855946c72cc6ba16ef42110
TryingPop <qotjdgns2001@gmail.com>
2025-05-24 21:38:12 +0900

    feat: 백준 연습용 문제 2025. 5 Study
    구현, 문자열 문제다.

---
+++ BaekJoon/00_000~09_999/4_000~4_999/4999_아!/4999_아!.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 5. 24
+이름 : 배성훈
+내용 : 아!
+    문제번호 : 4999번
+
+    문자열 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1643
+    {
+
+        static void Main1643(string[] args)
+        {
+
+            using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+
+            string input = sr.ReadLine();
+            int prev = input.Length;
+
+            input = sr.ReadLine();
+            Console.Write(prev < input.Length ? "no" : "go");
+        }
+    }
+}
