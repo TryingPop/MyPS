8ae5a9c5bafeaa3e8b4aa89f8117fb78f0bdebad
TryingPop <qotjdgns2001@gmail.com>
2025-05-29 16:29:15 +0900

    fix: 최신화 2025. 5 Study
    풀이 추가

---
+++ BaekJoon/etc/etc_1653.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.etc
+{
+    internal class etc_1653
+    {
+
+        static void Main1653(string[] args)
+        {
+
+            // 수열과 쿼리 20 - 16903번
+            // Trie
+            // 나중에 해결할꺼!
+        }
+    }
+}
