084f45ab23fdf597f81b1c16cb154800a1b984ff
TryingPop <qotjdgns2001@gmail.com>
2025-06-06 22:28:04 +0900

    feat; 백준 연습용 문제 2025. 6 Study
    두 포인터 문제다.

---
+++ BaekJoon/10_000~19_999/15_000~15_999/15831_준표의_조약돌/15831_준표의_조약돌.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 6. 6
+이름 : 배성훈
+내용 : 준표의 조약돌
+    문제번호 : 15831번
+
+    두 포인터 문제다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1682
+    {
+
+        static void Main1682(string[] args)
+        {
+
+            int N, W, B;
+            string str;
+
+            Input();
+
+            GetRet();
+
+            void GetRet()
+            {
+
+                int l = 0;
+                int r = -1;
+                int b = 0, w = 0;
+                int ret = 0;
+
+                while (r < N)
+                {
+
+                    if (b <= B)
+                    {
+
+                        r++;
+                        if (r == N) break;
+
+                        if (str[r] == 'W') w++;
+                        else b++;
+                    }
+                    else
+                    {
+
+                        if (str[l++] == 'W') w--;
+                        else b--;
+                    }
+
+                    if (b <= B) ret = Math.Max(ret, b + w);
+                }
+
+                if (ret < W + B) ret = 0;
+                Console.Write(ret);
+            }
+
+            void Input()
+            {
+
+                using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+                string[] temp = sr.ReadLine().Split();
+                N = int.Parse(temp[0]);
+                B = int.Parse(temp[1]);
+                W = int.Parse(temp[2]);
+
+                str = sr.ReadLine();
+            }
+        }
+    }
+}
