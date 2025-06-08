228008a9b93f5cd03128ec6e338c988a78dfe265
TryingPop <qotjdgns2001@gmail.com>
2025-06-08 20:38:28 +0900

    feat: 백준 연습용 문제 2025. 6 Study
    dp 문제다.

---
+++ BaekJoon/10_000~19_999/16_000~16_999/16_700~16_799/16703_Matrice/16703_Matrice.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 1. 13
+이름 : 배성훈
+내용 : Matrice
+    문제번호 : 16703번
+
+    dp 문제다.
+    문제를 잘못 해석해서 한참을 고민했다.
+
+    #girls
+    ##areb
+    #.#est
+    #..#!!
+    #####!
+
+    예제를 보면
+    #
+    ##
+    형태가 3개,(↘ 형태라 한다.)
+
+    ##
+    #
+    형태가 1개,(↗)
+
+     #
+    ##
+    형태가 1개, (↙)
+
+    .
+    ..
+    형태가 1개, (↘)
+
+    !!
+     !
+    형태가 1개, (↖)
+    로 총 7개이다.
+
+    내부가 모두 같아야 한다!
+
+    #
+    ##
+    #.#
+    #..#
+    #####
+    는 내부에 다른게 있으므로 카운팅하지 않는다!
+
+    그래서 가능한 최대 크기로 찾으면 된다.
+    그러면 최대 크기 -1이 개수가 된다.
+
+    삼각형이 추가되는 경우를 보면 각 현재 점과 각 방향에 맞는 두 지점이 같은지 확인하고
+    같으면 늘려가는 식으로 찾으면 된다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1270
+    {
+
+        static void Main1270(string[] args)
+        {
+
+            int row, col;
+            int[][] dp;
+            int[][] board;
+
+            Solve();
+            void Solve()
+            {
+
+                Input();
+
+                GetRet();
+            }
+
+            void GetRet()
+            {
+
+                // ↙, ↗, ↖, ↘ 형태의 삼각형
+                // 여기서 방향은 가로 선분이 있는 곳을 표시한 것이다.
+                int[] dirR1 = { 0, -1, -1, 0 }, dirC1 = { -1, 0, -1, -1 };
+                int[] dirR2 = { -1, -1, -1, -1 }, dirC2 = { 0, 1, 0, -1 };
+                long ret = 0;
+
+                dp = new int[row + 2][];
+                for (int r = 0; r <= row + 1; r++)
+                {
+
+                    dp[r] = new int[col + 2];
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+
+                    Chk(i);
+                }
+
+                Console.Write(ret);
+
+                void Chk(int _dir)
+                {
+
+                    for (int i = 0; i < dp.Length; i++)
+                    {
+
+                        Array.Fill(dp[i], 1);
+                    }
+
+                    for (int r = 1; r <= row; r++)
+                    {
+
+                        for (int c = 1; c <= col; c++)
+                        {
+
+                            if (board[r + dirR1[_dir]][c + dirC1[_dir]] == board[r][c] && board[r][c] == board[r + dirR2[_dir]][c + dirC2[_dir]])
+                            {
+
+                                dp[r][c] = Math.Min(dp[r + dirR1[_dir]][c + dirC1[_dir]], dp[r + dirR2[_dir]][c + dirC2[_dir]]) + 1;
+                                ret += dp[r][c] - 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            void Input()
+            {
+
+                StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+                string[] size = sr.ReadLine().Split();
+                row = int.Parse(size[0]);
+                col = int.Parse(size[1]);
+
+                board = new int[row + 2][];
+                board[0] = new int[col + 2];
+                for (int r = 1; r <= row; r++)
+                {
+
+                    board[r] = new int[col + 2];
+                    string temp = sr.ReadLine();
+                    for (int c = 0; c < col; c++)
+                    {
+
+                        board[r][c + 1] = temp[c];
+                    }
+                }
+
+                board[row + 1] = new int[col + 2];
+                sr.Close();
+            }
+        }
+    }
+
+#if other
+// #include <bits/stdc++.h>
+using namespace std;
+
+// #define st first
+// #define nd second
+// #define pb push_back
+// #define mp make_pair
+// #define klar(v) memset(v, 0, sizeof(v))
+// #define endl "\n"
+
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpii;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+const int maxn = 1100;
+
+char grid[maxn][maxn];
+int Xa[] = {-1, 0, -1, -1}, Ya[] = {0, -1, -1, 0};
+int Xb[] = {0, 1, 0, -1}, Yb[] = {-1, -1, -1, -1};
+ll dp[maxn][maxn];
+
+ll gemacht(char w, int n, int m, int k){
+	ll ret = 0;
+	//if(w != '#')return 0;
+	//if(k != 0)return 0;
+	klar(dp);
+	for(int i = 1; i <= n; i++)
+		for(int j = 1; j <= m; j++)
+			if(grid[i][j] == w){
+				dp[i][j] = min(dp[i+Ya[k]][j+Xa[k]], dp[i+Yb[k]][j+Xb[k]])+1;
+				//cout << i << " " << j << " " << dp[i][j] << " " << dp[i+Yb[k]][j+Xb[k]] << " " << i+Yb[k] << " " << j+Xb[k] << endl;
+			}
+	//cout << endl;
+	for(int i = 1; i <= n; i++)
+		for(int j = 1; j <= m; j++)
+			ret += max(dp[i][j]-1, 0LL);
+	return ret;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	int n, m;
+	cin >> n >> m;
+	for(int i = 1; i <= n; i++)
+		for(int j = 1; j <= m; j++)
+			cin >> grid[i][j]; 
+	ll ans = 0;
+	for(int i = 33; i <= 126; i++)
+		for(int j = 0; j < 4; j++)
+			ans += gemacht(char(i), n, m, j);
+	cout << ans << endl;
+	return 0;
+}
+#elif other2
+// #include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+// #define mp make_pair
+// #define x first
+// #define y second
+
+ll dp[1010][1010];
+
+void solve() {
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> A(n);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    ll ans = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (A[i][j] == A[i - 1][j] && A[i][j] == A[i][j - 1]) {
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + 1;
+            }
+            ans += dp[i][j];
+        }
+    }
+    memset(dp, 0, sizeof(dp));
+    for (int i = 1; i < n; i++) {
+        for (int j = m - 2; j >= 0; j--) {
+            if (A[i][j] == A[i - 1][j] && A[i][j] == A[i][j + 1]) {
+                dp[i][j] = min(dp[i][j + 1], dp[i - 1][j]) + 1;
+            }
+            ans += dp[i][j];
+        }
+    }
+
+    memset(dp, 0, sizeof(dp));
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 1; j < m; j++) {
+            if (A[i][j] == A[i + 1][j] && A[i][j] == A[i][j - 1]) {
+                dp[i][j] = min(dp[i][j - 1], dp[i + 1][j]) + 1;
+            }
+            ans += dp[i][j];
+        }
+    }
+
+    memset(dp, 0, sizeof(dp));
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = m - 2; j >= 0; j--) {
+            if (A[i][j] == A[i + 1][j] && A[i][j] == A[i][j + 1]) {
+                dp[i][j] = min(dp[i][j + 1], dp[i + 1][j]) + 1;
+            }
+            ans += dp[i][j];
+        }
+    }
+
+    cout << ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+    solve();
+    return 0;
+}
+#endif
+}
---
+++ BaekJoon/10_000~19_999/16_000~16_999/16_700~16_799/16703_Matrice/README.md
@@ -0,0 +1,153 @@
+# C#
+
+## 난이도 : 플레티넘 5
+
+## 알고리즘 분류
+  - 다이나믹 프로그래밍
+
+## 제한조건
+  - 시간 제한 : 1초
+  - 메모리 제한 : 512 MB
+
+## 문제
+Agent Sue Thomas and her son are looking for trinities in a grid. The word trinity is a neologism referring to a particular triangular (as the morpheme “tri” suggests) shape composed of cells in the grid.<br/>
+Each trinity is a result of taking a square-shaped area of the cells and removing all cells that lie either above or below one of the two diagonals of the area. The diagonal may be either the main diagonal (southeast-northwest direction) or the main antidiagonal (southwest-northeast direction). A valid trinity consists of at least three grid cells and all its cells contain the same character.<br/>
+
+
+## 입력
+The first input line contains two numbers N and M (1 ≤ N, M ≤ 1000), describing the number of rows and columns in the grid, respectively. Each of next N lines contains M characters, whose ASCII codes are between 33 and 126, inclusively.<br/>
+
+
+## 출력
+Output the number of different valid trinities in the input grid.<br/>
+
+
+## 예제 입력
+5 6<br/>
+\#girls<br/>
+\#\#areb<br/>
+\#\.\#est<br/>
+\#\.\.\#\!\!<br/>
+\#\#\#\#\#\!<br/>
+
+
+## 예제 출력
+7<br/>
+
+
+## 풀이
+예제를 보자.<br/>
+
+|#|g|i|r|l|s|
+|:---:|:---:|:---:|:---:|:---:|:---:|
+|#|#|a|r|e|b|
+|#|.|#|e|s|t|
+|#|.|.|#|!|!|
+|#|#|#|#|#|!|
+
+
+가능한 삼각형은 다음과 같다.<br/>
+
+|O|g|i|r|l|s|
+|:---:|:---:|:---:|:---:|:---:|:---:|
+|O|O|a|r|e|b|
+|#|.|#|e|s|t|
+|O|.|.|O|!|!|
+|O|O|#|O|O|!|
+
+
+|#|g|i|r|l|s|
+|:---:|:---:|:---:|:---:|:---:|:---:|
+|O|O|a|r|e|b|
+|O|.|#|e|s|t|
+|#|.|.|O|!|!|
+|#|#|O|O|#|!|
+
+
+|#|g|i|r|l|s|
+|:---:|:---:|:---:|:---:|:---:|:---:|
+|#|#|a|r|e|b|
+|#|O|#|e|s|t|
+|#|O|O|#|O|O|
+|#|#|#|#|#|O|
+
+
+이렇게 총 3개, 2개, 2개이다.<br/>
+삼각형을 이루는 모든 원소가 같아야 한다.<br/>
+
+
+가능한 삼각형의 형태를 보면 다음과 같이 4개이다.<br/>
+
+
+|O|X||X|O||O|O||O|O|
+|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
+|O|O||O|O||O|X||X|O|
+
+
+이를 왼쪽부터 각각 ↘, ↙, ↗, ↖ 화살표로 표현한다.<br/>
+이렇게 화살표를 선언한건 선분은 대각선을 뜻하고 화살표 방향은 가로가 2개인 곳을 나타낸다.<br/>
+↘ 화살표의 경우 대각선이 ＼형태이고 아래쪽에 2개가 있다는 뜻이다.<br/>
+
+
+↘형태의 삼각형부터 알아보자.<br/>
+가장 아래 꼭짓점을 기준으로 구분한다. 만약 가장 아래 점이 여러개인 경우 가장 오른쪽에 있는 점을 기준으로 한다.<br/>
+그러면 ↘형태에서는 다음 A 위치가 기준이 된다.<br/>
+
+|O|||
+|:---:|:---:|:---:|
+|O|O||
+|O|O|A|
+
+
+왼쪽 위에서부터 가장 큰 삼각형의 크기를 기록해간다.<br/>
+가장 큰 삼각형의 크기를 알면 해당 꼭짓점으로 하는 삼각형은 크기 - 1개 존재함을 알 수 있다.<br/>
+
+
+왼쪽 끝과 위쪽 끝 가장자리에 있는 것은 1임이 자명하다.<br/>
+예제로 크기롤 보면 다음과 같다.<br/>
+
+
+|#|g|i|r|l|s||1|1|1|1|1|1|
+|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
+|#|#|a|r|e|b||1||||||
+|#|.|#|e|s|t||1||||||
+|#|.|.|#|!|!||1||||||
+|#|#|#|#|#|!||1||||||
+
+
+이제 이외의 점은 왼쪽 위 대각선의 점과, 왼쪽 점과 같은지 먼저 확인해야 한다.<br/>
+같은 경우 왼쪽 위 대각선의 점과, 왼쪽 점 중 작은 값 + 1임을 알 수 있다.<br/>
+즉, 현재점 A에서 비교하는 점은 B가 된다.<br/>
+
+
+|O|||
+|:---:|:---:|:---:|
+|O|B||
+|O|B|A|
+
+
+이렇게 예제를 채워가면 다음과 같다.<br/>
+
+|#|g|i|r|l|s||1|1|1|1|1|1|
+|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
+|#|#|a|r|e|b||1|2|1|1|1|1|
+|#|.|#|e|s|t||1|1|1|1|1|1|
+|#|.|.|#|!|!||1|1|2|1|1|1|
+|#|#|#|#|#|!||1|2|1|1|2|1|
+
+
+2이상에 대해 1을 빼줘서 삼각형의 갯수를 세어주면 된다.<br/>
+해당 방법으로 ↘ 화살표인 삼각형의 갯수를 O(NM)에 모두 찾을 수 있다.<br/>
+
+
+↙, ↗, ↖인 경우도 비슷하게 찾아주면 된다.<br/>
+↙의 경우 현재지점 A에서 왼쪽과 위쪽 지점이 비교할 점 B가 된다.<br/>
+↗의 경우 현재지점 A에서 위쪽과 오른쪽 위 지점이 B가 된다.<br/>
+↖의 경우 현재지점 A에서 위쪽과 왼쪽 위 지점이 B가 된다.<br/>
+
+
+이렇게 찾아 누적하면 정답이 된다.<br/>
+
+
+## 문제 링크
+https://www.acmicpc.net/problem/16703
