30460f8853f32ae2e6f674d5511f304577a3bbbc
TryingPop <qotjdgns2001@gmail.com>
2025-06-10 17:54:31 +0900

    feat: 백준 연습용 문제 2025. 6 Study
    dp, 배낭 문제다.

---
+++ BaekJoon/20_000~29_999/26_000~26_999/26607_시로코와_은행털기/26607_시로코와_은행털기.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 6. 10
+이름 : 배성훈
+내용 : 시로코와 은행털기
+    문제번호 : 26607번
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1690
+    {
+
+        static void Main1690(string[] args)
+        {
+
+            int n, k, x;
+            (int a, int b)[] arr;
+
+            Input();
+
+            GetRet();
+
+            void GetRet()
+            {
+
+                bool[][] dp = new bool[k + 1][];
+                for (int i = 0; i <= k; i++)
+                {
+
+                    dp[i] = new bool[x * i + 1];
+                }
+
+                dp[0][0] = true;
+
+                for (int i = 0; i < k; i++)
+                {
+
+                    for (int j = i; j >= 0; j--)
+                    {
+
+                        for (int s = 0; s < dp[j].Length; s++)
+                        {
+
+                            if (dp[j][s])
+                                dp[j + 1][s + arr[i].a] = true;
+                        }
+                    }
+                }
+
+                for (int i = k; i < n; i++)
+                {
+
+                    for (int j = k - 1; j >= 0; j--)
+                    {
+
+                        for (int s = 0; s < dp[j].Length; s++)
+                        {
+
+                            if (dp[j][s])
+                                dp[j + 1][s + arr[i].a] = true;
+                        }
+                    }
+                }
+
+                int sum = k * x;
+                int ret = 0;
+                for (int i = 0; i < dp[k].Length; i++)
+                {
+
+                    if (dp[k][i])
+                    {
+
+                        int chk = (sum - i) * i;
+                        ret = Math.Max(ret, chk);
+                    }
+                }
+
+                Console.Write(ret);
+            }
+
+            void Input()
+            {
+
+                using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+                n = ReadInt();
+                k = ReadInt();
+                x = ReadInt();
+
+                arr = new (int a, int b)[n];
+                for (int i = 0; i < n; i++)
+                {
+
+                    arr[i] = (ReadInt(), ReadInt());
+                }
+
+                int ReadInt()
+                {
+
+                    int ret = 0;
+
+                    while (TryReadInt()) ;
+                    return ret;
+
+                    bool TryReadInt()
+                    {
+
+                        int c = sr.Read();
+                        if (c == '\r') c = sr.Read();
+                        if (c == '\n' || c == ' ') return true;
+                        ret = c - '0';
+
+                        while ((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                        {
+
+                            if (c == '\r') continue;
+                            ret = ret * 10 + c - '0';
+                        }
+
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
