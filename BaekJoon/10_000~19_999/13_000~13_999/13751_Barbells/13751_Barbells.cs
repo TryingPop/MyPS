42ad9359c9e53301f41d8c19972cb8176f6fc091
TryingPop <qotjdgns2001@gmail.com>
2025-06-15 21:02:08 +0900

    feat: 백준 연습용 문제 2025. 6 Study
    브루트포스 문제다.

---
+++ BaekJoon/10_000~19_999/13_000~13_999/13751_Barbells/13751_Barbells.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+날짜 : 2025. 6. 14
+이름 : 배성훈
+내용 : Barbells
+    문제번호 : 13751번
+
+    브루트포스 문제다.
+    탐색 경우는 3^14 x p = 400만 x 14 = 5600만이다.
+    브루트포스가 유효하다 판단했다.
+
+    가능한 정답의 수를 보면 왼쪽에 넣는 것을 기준으로 2^14 x p < 2만 x p = 28만 갯수만큼 된다.
+    배열로 접근하는 경우 정렬 후 중복처리 코드를 추가해줘야 한다.
+    해시셋으로 할 경우 정렬만 해주면 되기에 해시셋에 저장했다.
+*/
+
+namespace BaekJoon.etc
+{
+    internal class etc_1702
+    {
+
+        static void Main1702(string[] args)
+        {
+
+            int b, p;
+            int[] bar, plates;
+
+            Input();
+
+            GetRet();
+
+            void GetRet()
+            {
+
+                using StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 65536);
+                HashSet<int> ret = new((1 << 14) * b);  // 왼쪽에 포함되는건 (2^14) * b
+
+                DFS(0, 0, 0);
+
+                foreach (int item in ret.OrderBy(x => x))
+                {
+
+                    sw.Write(item);
+                    sw.Write('\n');
+                }
+
+                void DFS(int _dep, int _l, int _r)
+                {
+
+                    if (_dep == p)
+                    {
+
+                        if (_l != _r) return;
+
+                        for (int i = 0; i < b; i++)
+                        {
+
+                            ret.Add(bar[i] + _l + _r);
+                        }
+
+                        return;
+                    }
+
+                    DFS(_dep + 1, _l, _r);
+                    DFS(_dep + 1, _l + plates[_dep], _r);
+                    DFS(_dep + 1, _l, _r + plates[_dep]);
+                }
+            }
+
+            void Input()
+            {
+
+                using StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 65536);
+
+                b = ReadInt();
+                p = ReadInt();
+
+                bar = new int[b];
+                for (int i = 0; i < b; i++)
+                {
+
+                    bar[i] = ReadInt();
+                }
+
+                plates = new int[p];
+                for (int i = 0; i < p; i++)
+                {
+
+                    plates[i] = ReadInt();
+                }
+
+                int ReadInt()
+                {
+
+                    int ret = 0;
+
+                    while (TryReadInt()) ;
+                    return ret;
+
+                    bool TryReadInt()
+                    {
+
+                        int c = sr.Read();
+                        if (c == '\r') c = sr.Read();
+                        if (c == '\n' || c == ' ') return true;
+                        ret = c - '0';
+
+                        while ((c = sr.Read()) != -1 && c != ' ' && c != '\n')
+                        {
+
+                            if (c == '\r') continue;
+                            ret = ret * 10 + c - '0';
+                        }
+
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BaekJoon/10_000~19_999/13_000~13_999/13751_Barbells/README.md
@@ -0,0 +1,58 @@
+# C#
+
+## 난이도 : 골드 4
+
+## 알고리즘 분류
+  - 브루트포스 알고리즘
+  - 비트마스킹
+  - 백트래킹
+
+## 제한조건
+  - 시간 제한 : 2초
+  - 메모리 제한 : 512 MB
+
+## 문제
+Your local gym has b bars and p plates for barbells. In order to prepare a weight for lifting, you must choose a single bar, which has two sides. You then load each side with a (possibly empty) set of plates. For safety reasons, the plates on each side must balance; they must sum to the same weight. The combination of plates on either side might be different, but the total weight on either side must be the same. What weights are available for lifting?<br/>
+
+
+## 입력
+Each input will consist of a single test case. Note that your program may be run multiple times on different inputs. The first line of input contains two integers, b and p (1 ≤ b,p ≤ 14), representing the number of bars and plates. Then, there are b lines each containing a single integer x (1 ≤ x ≤ 108 ) which are the weights of the bars. After that, there are p lines each containing a single integer y (1 ≤ y ≤ 108 ) which are the weights of the plates.<br/>
+
+
+## 출력
+Output a sorted list of all possible lifting weights, one per line. There must be no duplicates.<br/>
+
+
+## 예제 입력
+2 5<br/>
+100<br/>
+110<br/>
+5<br/>
+5<br/>
+1<br/>
+4<br/>
+6<br/>
+
+
+## 예제 출력
+100<br/>
+110<br/>
+112<br/>
+120<br/>
+122<br/>
+130<br/>
+
+
+## 풀이
+플레이트를 양쪽에 놓거나 안놓는 경우 가능한 경우는 플레이트가 14개 이므로 3^14이다.<br/>
+그래서 브루트포스로 플레이트를 양쪽에 놓으면서 무게가 같은 경우를 찾았다.<br/>
+
+
+가능한 경우는 왼쪽을 기준으로만 보면 2^14개 있다.<br/>
+막대기가 14개 있으므로 가능한 무게는 14 x 2^14개가 가능하다.<br/>
+이후 무게 순으로 정렬하고 중복한 경우 건너뛰는 식으로 찾아도 된다.<br/>
+그래서 HashSet에 저장하면 알아서 중복 확인하므로 정렬만 한 뒤 원소를 하나씩 읽어 제출했다.<br/>
+
+
+## 문제 링크
+https://www.acmicpc.net/problem/13751
